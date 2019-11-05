95f43a00e8c9bdb84aaf497fee07a0c96c88c0ab
Carlos Alberto Noronha <beto_noronha@live.com>
2019-11-05 10:51:40 -0300

    Update env.js

--- bdc-oauth-app/src/assets/env.js
+++ bdc-oauth-app/src/assets/env.js
@@ -2,7 +2,7 @@
     window.__env = window.__env || {};
 
     // API url Oauth
-    window.__env.urlOauth = 'http://oauth.dpi.inpe.br/api/oauth';
+    window.__env.urlOauth = 'http://localhost:5001/oauth';
     // URL APM SERVER
     window.__env.urlAPMServer = 'http://127.0.0.1:8200/';
 
